c1f96a5f1dd30557c755107a18dba4e61033bb80
Siddhant Sharma <sharmasiddhant301@gmail.com>
2023-11-27 22:36:37 -0800

    checkpoint

--- demo/fabfile.py
+++ demo/fabfile.py
@@ -18,7 +18,7 @@ def local(ctx, debug=True):
         'workers': 1,
         'rate': 10_000,
         'tx_size': 512,
-        'duration': 20,
+        'duration': 25,
     }
     node_params = {
         'timeout_delay': 200,  # ms
